177b0286867235280c9c3d403a3035d0a12e1beb
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-11-14 20:18:37 +0200

    Add header

--- Test.c
+++ Test.c
@@ -1,33 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// Calculate length without dirrect accessing with []
-int my_strlen(const char *str)
-{
-	const char* start = str;
-	int length = 0;
-
-	while(*start)
-	{
-		length++;
-		start++;
-	}
-
-	return length;
-}
-
-void mirror(char *s)
-{
-	int length = my_strlen(s);
-	
-	for(int i = 0; i < length / 2; ++i)
-	{
-		char tmp = *(s + i);
-		*(s + i) = *(s + length - 1 - i);
-		*(s + length-1-i) = tmp;
-	}
-}
+#include "Test.h"
 
 int main()
 {
